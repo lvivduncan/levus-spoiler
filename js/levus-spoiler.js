fda24490a23946390e17359431553b9429545639
lvivduncan <lvivduncan@gmail.com>
2020-12-06 14:42:45 +0200

    small edit

--- js/levus-spoiler.js
+++ js/levus-spoiler.js
@@ -2,8 +2,10 @@
 function levusSpoiler(selector, word = "spoiler"){
 
     document.querySelectorAll(selector).forEach(item => {
+        
         // основний блок
         item.style.cssText = 'position:relative;overflow:hidden;height:38px';
+
         // додаємо кнопку
         const el = document.createElement('div');
         el.style.cssText = 'position:absolute;top:0;left:0;width:100%;padding:10px;text-align:center;background:white;color:red;cursor:pointer;';
