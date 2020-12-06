b31e76064b8e9f76bd7e3482bc51195d305b4f3a
lvivduncan <lvivduncan@gmail.com>
2020-12-06 14:40:46 +0200

    add min.js

--- js/levus-spoiler.ie5.js
+++ js/levus-spoiler.ie5.js
@@ -1,16 +1,16 @@
+// 6-12-2020
 "use strict";
 
 function levusSpoiler(selector) {
   var word = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "spoiler";
   document.querySelectorAll(selector).forEach(function (item) {
-    // основний блок
-    item.style.cssText = 'position:relative;overflow:hidden;height:38px'; // додаємо кнопку
+    item.style.cssText = 'position:relative;overflow:hidden;height:38px';
 
     var el = document.createElement('div');
     el.style.cssText = 'position:absolute;top:0;left:0;width:100%;padding:10px;text-align:center;background:white;color:red;cursor:pointer;';
     el.innerHTML = "<span class=\"levus-spoiler\">".concat(word, "</span>");
     el.className = 'levus-spoiler';
-    item.append(el); // клік по кнопці
+    item.append(el);
 
     item.addEventListener('click', function (e) {
       if (e.target.className == 'levus-spoiler') {
